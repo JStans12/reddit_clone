da89c1ca928dfe67fb1669fa248c65a22ad4e7f8
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-06 14:20:14 -0700

    changed reddit state to a before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :set_reddit_state
   helper_method :current_user
+
+  def set_reddit_state
+    @reddit_state = (rand(9000) + 1000).to_s
+    session[:reddit_state] = @reddit_state
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,4 @@
 class DashboardController < ApplicationController
   def index
-    @state = (rand(9000) + 1000).to_s
-    session[:reddit_state] = @state
   end
 end
