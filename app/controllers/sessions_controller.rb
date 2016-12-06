d11c983a09cab3175218668644607772655f33b0
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-06 14:02:30 -0700

    added sad path if codes don't match

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,15 @@ class SessionsController < ApplicationController
   def create
     if session[:reddit_state] = params[:state]
 
-      tokens = RedditOauthService.request_tokens(params[:code])
+      tokens    = RedditOauthService.request_tokens(params[:code])
       user_data = RedditMeService.request_user_data(tokens)
-      user = User.find_or_create_user(user_data, tokens)
+      user      = User.find_or_create_user(user_data, tokens)
 
       session[:user_id] = user.id
 
       redirect_to root_path
+    else
+      render file: 'public/404', layout: false
     end
   end
 end
