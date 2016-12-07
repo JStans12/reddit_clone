288b847b9c0b547853e1f6a758b324f651963d30
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-07 13:49:32 -0700

    request user data service tested

---
+++ spec/services/reddit_me_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe RedditServices do
+  context "#request_user_data" do
+    it "returns all data for a specific user" do
+      VCR.use_cassette("request_user_data") do
+        
+        user = RedditServices.new(ENV['user_test_token']).request_user_data
+
+        expect(user).to have_key(:id)
+        expect(user).to have_key(:name)
+        expect(user).to have_key(:link_karma)
+      end
+    end
+  end
+end
