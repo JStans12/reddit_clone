3ed4c555a2eb4b9ef55d676423098c60b47bea36
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-07 15:02:30 -0700

    removed a webmock problem

--- spec/models/subreddit_spec.rb
+++ spec/models/subreddit_spec.rb
@@ -4,7 +4,6 @@ describe RedditServices do
   context "#subreddits" do
     it "returns all subreddits for logged in user" do
       VCR.use_cassette("#subreddits") do
-        WebMock.allow_net_connect!
 
         subreddits = Subreddit.by_user(ENV['user_test_token2'])
         subreddit = subreddits.first
